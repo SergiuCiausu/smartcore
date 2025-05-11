8c670ffb8a85780a1e54e321363e8dba4426cb00
SergiuCiausu <ciausu.sergiu@gmail.com>
2025-05-11 14:11:12 +0300

    wat

--- app/cursul-1/Sectiuni/Sectiune1.tsx
+++ app/cursul-1/Sectiuni/Sectiune1.tsx
@@ -9,7 +9,7 @@ const Sectiune1 = () => {
             <h3 className="w-[576px] text-center">Cei mai prosperi angajați lucrează din plăcere</h3>
             <div>
                 <p>
-                Munca a devenit, abcd pentru mulți dintre noi, o rutină obositoare, dominată de liste interminabile de „to-do”-uri, presiuni externe și o motivație care scade de la o zi la alta. Una dintre cele mai mari probleme moderne este lipsa plăcerii în activitățile zilnice: <strong>lucrăm din obligație, nu din dorință</strong>. 
+                Munca a devenit, pentru mulți dintre noi, o rutină obositoare, dominată de liste interminabile de „to-do”-uri, presiuni externe și o motivație care scade de la o zi la alta. Una dintre cele mai mari probleme moderne este lipsa plăcerii în activitățile zilnice: <strong>lucrăm din obligație, nu din dorință</strong>. 
                 </p>
                 <p>
                 Acest curs pornește de la ideea că <strong>ceea ce lipsește din majoritatea sarcinilor noastre este o formă de joc</strong> — acel sentiment de progres, de provocare, de recompensă și de satisfacție care face ca timpul să treacă mai repede și rezultatele să vină mai ușor.
